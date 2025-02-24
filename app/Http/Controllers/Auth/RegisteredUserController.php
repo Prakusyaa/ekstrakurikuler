d6710716068359f2d639a22622975becee150593
Muhammad Anindya Prakusya <muhammadanindyaprakusya@gmail.com>
2025-02-24 22:02:15 +0700

    verif

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'role' => 'siswa',
+            'verif' => 'belum'
         ]);
 
         event(new Registered($user));

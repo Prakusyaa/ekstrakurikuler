0bb49cc0f0d89aeec7b105b86d89f75f1743d655
Muhammad Anindya Prakusya <muhammadanindyaprakusya@gmail.com>
2025-03-13 00:08:10 +0700

    migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id(); // bigint unsigned
+            $table->id();
             $table->string('name');
             $table->enum('role', ['siswa', 'guru', 'admin']);
             $table->enum('verif', ['unverified', 'verified']);

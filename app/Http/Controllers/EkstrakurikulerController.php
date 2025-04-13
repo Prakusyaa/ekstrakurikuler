a29cbb873cf2492571f70295d3db0d2ee2d2ab4b
Muhammad Anindya Prakusya <muhammadanindyaprakusya@gmail.com>
2025-04-13 17:06:57 +0700

    search function

--- app/Http/Controllers/EkstrakurikulerController.php
+++ app/Http/Controllers/EkstrakurikulerController.php
@@ -4,19 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Ekstrakurikuler;
+use Illuminate\Support\Facades\Auth;
 
 class EkstrakurikulerController extends Controller
 {
     public function index(Request $request)
     {
         $search = $request->input('search');
+        $ekstrakurikuler = $this->search($search);
 
-        $ekstrakurikuler = Ekstrakurikuler::when($search, function ($i) use ($search) {
-            $i->where('nama', 'like', "%{$search}%");
-        })->get();
-
-        if (auth()->check()) {
-            $anggota = auth()->user()->anggota()->pluck('ekskul_id')->toArray();
+        if (Auth::check()) {
+            $anggota = Auth::user()->anggota()->pluck('ekskul_id')->toArray();
         } else {
             $anggota = [];
         }
@@ -24,10 +22,17 @@ class EkstrakurikulerController extends Controller
         return view('ekstrakurikuler.daftar_ekstrakurikuler', compact('ekstrakurikuler', 'anggota'));
     }
 
+    private function search($search)
+    {
+        return Ekstrakurikuler::when($search, function ($query) use ($search) {
+            $query->where('nama', 'like', "%{$search}%");
+        })->get();
+    }
+
     public function show($id)
     {
         $ekskul = Ekstrakurikuler::with('berita.user')->findOrFail($id);
-        $user = auth()->user();
+        $user = Auth::user();
 
         return view('ekstrakurikuler.detail', [
             'ekskul' => $ekskul,
